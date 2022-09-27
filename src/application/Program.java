cc2dc85b5c651be41cca6bdd118724ad91e85d51
Gustavo Alves Abreu <113162142+Gustavoabreu94@users.noreply.github.com>
2022-09-27 14:55:56 -0300

    ChessLayer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,16 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Position pos = new Position(1,1);
+		ChessMatch chessMatch = new ChessMatch();
 		
-		System.out.println(pos);
+		UI.printBoard(chessMatch.getpieces());
+		
 	}
 
 }
